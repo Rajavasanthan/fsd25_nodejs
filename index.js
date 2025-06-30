103bf47e59b50c269091e35c5367a6301188f857
rajavasanthan <rajavasanthan@gmail.com>
2025-06-30 19:11:39 +0530

    password

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const app = express();
 const { connect } = require("./db_config");
 const { Product } = require("./Product_Schema");
 const cors = require("cors");
-const dotenv = require("dotenv").config()
+const { User } = require("./User_Schema");
+const bcrypt = require("bcryptjs");
+const dotenv = require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
     origin: "*",
   })
 );
-
-
 
 (async () => {
   await connect();
@@ -61,6 +61,52 @@ app.delete("/product/:id", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Webserver running");
+app.post("/register", async (req, res) => {
+  try {
+    // Email, Password
+
+    // Generate Salt
+    const salt = bcrypt.genSaltSync(10);
+
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const user = new User({
+      email: req.body.email,
+      password: hash,
+    });
+    await user.save();
+    res.json({ message: "User Created" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    // Find If the Email present in Database
+    // If Email present then hash the password and compare it to users's hash
+    // if hash is matching then make him login.
+
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const checkPassword = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if (checkPassword) {
+        res.json({ message: "Correct Password" });
+      } else {
+        res.status(401).json({
+          message: "Incorrect Password",
+        });
+      }
+    } else {
+      res.status(401).json({ email: "User Not Found" });
+    }
+  } catch (error) {}
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Webserver running", port);
 });
